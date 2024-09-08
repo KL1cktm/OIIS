3844cc42c4b4bf27e3db39751a3082da305a2814
KL1cktm <urgzhenyok@mail.ru>
2024-09-08 17:46:22 +0300

    Brightness Equalization work for 2 image

--- lab3/src/com/example/brightCorrect/BrightCorrection.java
+++ lab3/src/com/example/brightCorrect/BrightCorrection.java
@@ -14,7 +14,8 @@ public class BrightCorrection {
             BufferedImage bufferedImage1 = ImageIO.read(file1);
             BufferedImage bufferedImage2 = ImageIO.read(file2);
             brightnessEqualization(bufferedImage1,bufferedImage2);
-            ImageIO.write(bufferedImage2, "jpg", new File("image_adjusted.jpg"));
+            ImageIO.write(bufferedImage1, "jpg", new File("image_adjusted1.jpg"));
+            ImageIO.write(bufferedImage2, "jpg", new File("image_adjusted2.jpg"));
             System.out.println("Программа завершена успешно!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,20 +25,32 @@ public class BrightCorrection {
     private void brightnessEqualization(BufferedImage image1,BufferedImage image2) {
         int brightLevel1 = getBrightnessLevel(image1);
         int brightLevel2 = getBrightnessLevel(image2);
-        int brightDifference = brightLevel1 - brightLevel2;
-        for (int x=0;x<image2.getWidth();x++){
-            for (int y=0;y<image2.getHeight();y++) {
-                int rgb = image2.getRGB(x,y);
+        int brightDifference = Math.abs(brightLevel1 - brightLevel2)/2;
+        int brightDif1, brightDif2;
+        if (brightLevel1 > brightLevel2) {
+            brightDif1 = brightDifference * -1;
+            brightDif2 = brightDifference;
+        } else {
+            brightDif2 = brightDifference * -1;
+            brightDif1 = brightDifference;
+        }
+        brightnessCorrect(image1,brightDif1);
+        brightnessCorrect(image2,brightDif2);
+    }
+
+    private void brightnessCorrect(BufferedImage image, int brightDifference) {
+        for (int x=0;x<image.getWidth();x++){
+            for (int y=0;y<image.getHeight();y++) {
+                int rgb = image.getRGB(x,y);
                 Color color = new Color(rgb);
                 int red = Math.min(Math.max(color.getRed()+brightDifference,0),255);
                 int green = Math.min(Math.max(color.getGreen()+brightDifference,0),255);
                 int blue = Math.min(Math.max(color.getBlue()+brightDifference,0),255);
                 Color newColor = new Color(red,green,blue);
-                image2.setRGB(x,y,newColor.getRGB());
+                image.setRGB(x,y,newColor.getRGB());
             }
         }
     }
-
     private int getBrightnessLevel(BufferedImage image) {
         int x = image.getWidth();
         int y = image.getHeight();
